73c9d7af69db10afb98657691adc63f151191ca3
Frigyes Elemér <125876771+drt09@users.noreply.github.com>
2023-11-10 19:44:33 +0100

    Update other.js

--- assets/js/other.js
+++ assets/js/other.js
@@ -5,14 +5,3 @@ function execCmd(command, value = null) {
 function toggleItalic() {
     execCmd('italic');
 }
-
-function changeFont() {
-    document.execCommand("fontSize", false, num);
-    var fontElements = document.getElementsByTagName("font");
-    for (var i = 0, len = fontElements.length; i < len; ++i) {
-        if (fontElements[i].size == "7") {
-            fontElements[i].removeAttribute("size");
-            fontElements[i].style.fontSize = "30px";
-        }
-    }
-}
